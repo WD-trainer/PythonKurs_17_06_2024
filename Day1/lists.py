1b9f440d57161f57d9e256168abe5b1553f883fa
Wojciech Dudzik <wojtekdudzik@o2.pl>
2024-06-17 10:01:13 +0200

    Second task

--- Day1/lists.py
+++ Day1/lists.py
@@ -46,3 +46,47 @@ if __name__ == '__main__':
 
     #  Stwórz dwie listy. Każda z list ma zawierać 10 liczb losowych z zakresu 1-10. Wypisz listy na konosole raz z uzyciem petli
     #  drugi raz z uzyciem funkcji print
+    random_list_1 = []
+    random_list_2 = []
+    for i in range(10):
+        random_list_1.append(random.randint(1, 10))
+        random_list_2.append(random.randint(1, 10))
+
+    print(random_list_1)
+
+    for index, element in enumerate(random_list_2):
+        print(f'Presenting: {index} : {element}')
+
+    ################################
+
+
+    list1 = [0,1,2]
+    list2 = [3,4,5]
+
+    list1[0] = 10
+
+    # for e in list2:
+    #     list1.append(e)
+
+    list1.extend(list2)
+    print(list1)
+
+    joined_list = list1 + list2
+    print(joined_list)
+
+    last_element = joined_list.pop()
+    print(joined_list)
+    print(f'Last element was: {last_element}')
+
+    joined_list.remove(3)
+    print(joined_list)
+
+    random_list_3 = []
+    for i in range(10):
+        random_list_3.append(random.randint(1, 10))
+
+    random_list_4 = [random.randint(1, 10) for i in range(10)]
+    print(random_list_4)
+
+    # Napisz kod który umieści w liście 10 kolejnych potęg liczby 2 uzywajac list składanych. 2 ** 2 = 4, 2 ** 3 = 8
+
