b9b853d91d122e56aacb0b0c450af4b221f3c221
Wojciech Dudzik <wojtekdudzik@o2.pl>
2024-06-18 11:37:15 +0200

    Objects task 2

--- Day2/classes_and_objects.py
+++ Day2/classes_and_objects.py
@@ -53,5 +53,92 @@ print("*" * 45)
 #     Stwórz klasę "Samochod" posiadającą pola "marka", "model", "rejestracja".
 #     Klasa ta powinna zawierać też metodę "wyswietl" wypisującą dane z obiektu na konsoli
 #     Stwórz dwa obiekty tej klasy i korzystajac  z metody "wyświetl" wyswietl na konsoli ich zawartość.
+class Samochod:
+    def __init__(self, marka:str, model:str, kierowca: Osoba, rejestracja:str = "DOMYSLNA"):
+        self.__marka = marka
+        self.model = model
+        self.rejestracja = rejestracja
+        self.kierowca = kierowca
+
+    def wyswietl(self):
+        print(f"Samochod: {self.__marka}, {self.model}, {self.rejestracja}, {self.kierowca}")
+
+    def __str__(self):
+        return f'Samochod: {self.__marka}, {self.model}, {self.rejestracja}, {self.kierowca}'
+
+    def __repr__(self):
+        return f'Samochod("{self.__marka}", "{self.model}", "{self.rejestracja}")'
+
+    def _funckje_dla_mechanika(self):
+        print("Naprawiam")
+
+
+s1 = Samochod("Opel", "Astra", o)
+s2 = Samochod("Audi", "R8", kierowca=o2, rejestracja="KR12345")
+
+s1.wyswietl()
+s2.wyswietl()
+
+napis_z_samochod =  str(s1)
+print(napis_z_samochod)
+
+s1._funckje_dla_mechanika()
+# print(s1.__marka) # tu bedzie blad
+
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
+
+    def get_radius(self):
+        print("Get radius")
+        return self._radius
+
+    def set_radius(self, value):
+        print("Set radius")
+        self._radius = value
+
+    radius = property(fget=get_radius,
+                      # fset=set_radius,
+                      doc="Radius property of circle")
+
+kolo = Circle(20)
+
+print(kolo.radius)
+try:
+    kolo.radius = 30
+except Exception as err:
+    print(err)
+
+
+class Rectangle:
+    def __init__(self, a:int, b:int):
+        self._a = a
+        self.b = b
+
+    @property
+    def a(self):
+        print("Get a")
+        return self._a
+
+    # @a.setter
+    # def a(self, value):
+    #     print("Set a")
+    #     self._a = value
+
+
+r = Rectangle(4,2)
+r.b = 8
+# r.a = 30
+print(f'Rectangle {r.a}, {r.b}')
+
+
+# Stwórz klasę Zawodnik posiadającą pola wzrost i masa, imie. Pola te mają być uzupełniane przy tworzeniu obiektu.
+# stworz atrybut BMI który będzie tylko do odczytu
+# Powołaj do życia obiekt tej klasy i wyświetl na konsoli obliczone BMI.
+# Wzrost jest atrybutem chronionym (__wzrost)
+# Waga może być zmieniana ale też jako atrybut z wykorzystaniem dekoratora @property
+# wzor na bmi = masa / (wzrost ** 2)   wzrost podany w metrach 1.84
+
+
 
 
