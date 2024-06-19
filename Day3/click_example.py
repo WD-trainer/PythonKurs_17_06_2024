bbbf105cc15b7e1664d4113de4505a2022e9af60
Wojciech Dudzik <wojtekdudzik@o2.pl>
2024-06-19 09:12:39 +0200

    Click task

---
+++ Day3/click_example.py
@@ -0,0 +1,29 @@
+# pip install click
+
+import os
+import click ### https://github.com/pallets/click
+
+### opakowanie tej funkcji przy pomocy bibloteki click
+
+def znajdz_pliki_i_katalogi(katalog_startowy: str, fraza: str):
+    """
+    Ta funckja zajmuje sie znajdowaniem katalowgo i plikow. Ignoruje wielkosci liter
+    Parameters
+    ----------
+    katalog_startowy - katalog od ktorego zaczniemy przeszukiwanie
+    fraza - fraza ktora jest wyszukiwana w nazwach plikow i katalogow
+
+    Returns
+    -------
+
+    """
+    znalezione_elementy = []
+
+    fraza = fraza.lower()
+
+    for root, dirs, files in os.walk(katalog_startowy):
+        for element in dirs + files:
+            if fraza in element.lower():
+                znalezione_elementy.append(os.path.join(root, element))
+
+    print(znalezione_elementy)
